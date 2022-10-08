49811151dbe7bf8324e660cd5da70650e7fcfa41
devarshee <ddevarshee12@gmail.com>
2022-10-08 11:18:20 +0530

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = 3030 || process.env.PORT;
 const connectDB = require("./db/connect")
 const schemes = require("./routes/schemes");
+const cors = require("cors");
+
+
+const corsOptions = {
+    // origin: process.env.ALLOWED_CLIENTS.split(',')
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}
+// console.log(corsOptions.origin);
+
+app.use(cors(corsOptions));
 
 
 app.use(express.static("./public"));
